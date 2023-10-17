346973ee10e2c094eeb27b94d53eef32a93e129c
Szymon Paluch <sp@riot.agency>
2023-10-17 13:14:42 +0200

    null

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Document is ready!");
-    // Add your JS logic here
+    var scrollArrow = document.getElementById('scroll-arrow');
+
+    scrollArrow.addEventListener('click', function() {
+        window.scrollBy({
+            top: window.innerHeight * 0.75, // scroll down by the height of the viewport
+            behavior: 'smooth'
+        });
+
+        // Hide the arrow after it is clicked
+        this.style.visibility = 'hidden';
+    });
+
+    window.addEventListener('scroll', function() {
+        // Hide the arrow when the page is scrolled
+        scrollArrow.style.visibility = 'hidden';
+    });
 });
